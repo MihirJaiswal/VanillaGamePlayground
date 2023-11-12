2ea0e7581a0481db2abd9efdfa6e2f3857cd1532
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-12 10:23:42 +0530

    added game 3

--- game3/game3.js
+++ game3/game3.js
@@ -1,16 +1,48 @@
-const square = document.querySelectorAll('.square')
+const squares = document.querySelectorAll('.square')
 const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left') 
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 10
+let timerId= null
 
 function randomSquare() {
-    square.forEach(square => {
+    squares.forEach(square => {
         square.classList.remove('mole')
     })
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    console.log(randomPosition)
-    console.log(Math.floor(Math.random() * 9))
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
 }
-randomSquare()
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole() {
+    
+    timerId= setInterval(randomSquare, 500)
+}
+ moveMole()
+
+ function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('Game Over, Your final score is '+  result)
+    }
+ }
+
+ let countDownTimerId = setInterval(countDown, 1000)
