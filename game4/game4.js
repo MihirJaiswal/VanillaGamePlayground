d31ff4b0f767c6de87c70dc306741eec87c718cf
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-13 16:54:34 +0530

    added game 4

--- game4/game4.js
+++ game4/game4.js
@@ -1,6 +1,20 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
+const boardWidth = 560
+const boardHeight = 300
+const ballDiameter = 20 
+let timerId
+let xDirection = 2 
+let yDirection = 2
+let score = 0
+
+const userStart = [230, 10]
+let currentPosition = userStart
+
+const ballStart = [270, 30]
+let ballCurrentPosition = ballStart
 
 //create block 
 class Block {
@@ -47,3 +61,134 @@ console.log(blocks[0])
 }
 
 addBlocks() 
+
+//creating player
+
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//draw user
+function drawUser(){
+    user.style.left = currentPosition[0] + 'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
+
+//draw ball
+function drawBall(){
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//moveuser
+function moveUser(e) {
+    switch(e.key){
+        case 'ArrowLeft':
+            if(currentPosition[0] > 0){
+                currentPosition[0] -= 10
+                drawUser()
+            }
+           
+            break;
+            case 'ArrowRight':
+            if(currentPosition[0] < boardWidth - blockWidth ){
+                currentPosition[0] += 10
+                drawUser()
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//move ball
+function moveBall(){
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    checkForCollision()
+}
+
+timerId = setInterval(moveBall,30)
+
+
+//check for collisions
+
+function checkForCollision(){
+    //check for block collision
+    for(let i = 0; i < blocks.length; i++){
+        if(
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i]. bottomRight[0]) &&
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ){
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+            changeDirection()
+            score++
+            scoreDisplay.innerHTML = score
+
+
+            //check for the win
+            if(blocks.length === 0){
+                scoreDisplay.innerHTML = 'you win'
+                clearInterval(timerId)
+                document.removeEventListener('keydown', moveUser)
+            }
+        }
+    }
+
+
+
+
+    //check for wall collision 
+    if(
+     ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
+     ballCurrentPosition[1] >= (boardHeight - ballDiameter)||
+     ballCurrentPosition[0] <= 0
+     ) {
+        changeDirection()
+    }
+
+
+    
+    //check for user collision
+    if(
+        (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth )&&
+        (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)){
+            changeDirection()
+        }
+
+
+    //check for gameover
+    if(ballCurrentPosition[1] <= 0){
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'you  lose'
+        document.removeEventListener('keydown', moveUser)
+    }
+}
+
+function changeDirection(){
+    if(xDirection === 2 && yDirection ===2){
+        yDirection = -2
+        return
+    }
+    if(xDirection === 2 && yDirection === -2){
+        xDirection = -2
+        return
+    }
+    if(xDirection === -2 && yDirection === -2){
+        yDirection = 2
+        return
+    }
+    if(xDirection === -2 && yDirection === 2){
+        xDirection = 2
+        return
+    }
+}
