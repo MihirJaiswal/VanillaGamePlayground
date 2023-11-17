fe95bdbf8aa87d26204f590dfbdb44777b3300eb
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-17 12:52:17 +0530

    game 5

---
+++ game5/app.js
@@ -0,0 +1,55 @@
+for(x=0; x<200; x++){
+    var blocks = document.createElement('div');
+    blocks.className = "blocks";
+
+    document.querySelector('.board').appendChild(blocks);
+}
+
+const board = document.querySelector('.board')
+let squares = Array.from(document.querySelectorAll('.blocks'))
+const scoreDisplay = document.querySelector('#score')
+const startBtn = document.querySelector('#start-button')
+const width = 10
+
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  
+  console.log(theTetrominoes)
+  let currentPosition = 4
+
+
